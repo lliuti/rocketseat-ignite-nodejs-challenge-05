2e92061dbc7d2388e6ef04c74cbf9c11a56e1bf5
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-02 22:12:59 -0300

    Initial commit

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,25 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  name: string;
+  email: string;
+}
+
+class CreateUserUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ email, name }: IRequest): User {
+    const alreadyExists = this.usersRepository.findByEmail(email);
+
+    if (alreadyExists) {
+      throw new Error(`User ${name} already exists`);
+    }
+
+    const user = this.usersRepository.create({ name, email });
+
+    return user;
+  }
+}
+
+export { CreateUserUseCase };
