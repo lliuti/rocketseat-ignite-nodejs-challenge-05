ea9569160b133dcbb8d357881f66569c33cef2a5
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-02 22:50:24 -0300

    Importing swagger.json and swaggerUi

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
+import swaggerJson from "../swagger.json";
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
 
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerJson));
 
 app.use("/users", usersRoutes);
 
